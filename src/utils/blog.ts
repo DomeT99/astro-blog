da9e775dfc0f1908034267280cd794e99e8ca2fc
Domenico Tenace <domenicotenace99@gmail.com>
2023-09-09 16:04:37 +0200

    ref:refactoring code base

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -1,22 +1,3 @@
 import { getCollection } from "astro:content";
 
 export const fetchCollection = async () => await getCollection("post");
-
-async function getStaticPaths() {
-  const posts = await fetchCollection();
-
-  return posts.map((post) => {
-    return {
-      params: {
-        slug: post.slug,
-      },
-      props: {
-        post,
-      },
-    };
-  });
-}
-
-export function getPaths() {
-  return getStaticPaths();
-}
