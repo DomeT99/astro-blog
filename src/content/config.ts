71f0acf65953e6662752384eef312f406ff285ea
Domenico Tenace <domenicotenace99@gmail.com>
2023-09-15 11:44:40 +0200

    feat: order post by date

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,9 +3,9 @@ import { z, defineCollection } from "astro:content";
 const post = defineCollection({
   schema: z.object({
     title: z.string(),
-    date: z.string(),
+    date: z.date(),
     description: z.string(),
-    devLink: z.string().url().optional()
+    devLink: z.string().url().optional(),
   }),
 });
 
