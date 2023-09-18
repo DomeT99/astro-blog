99c0523fc7f12510caf07e1211508ef92916913b
Domenico Tenace <domenicotenace99@gmail.com>
2023-09-18 19:31:16 +0200

    fix: general fix

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -21,6 +21,10 @@ export const socialButtons: SocialButton[] = [
 
 export const menuOptions: MenuOptions[] = [
   {
+    label: "HOME",
+    url: "/",
+  },
+  {
     label: "BLOG",
     url: "/blog",
   },
